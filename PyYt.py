2b4873bd497159179d7908a6d9755ec3553570a4
Mohamad_Alkabbani <alkabbani@webetel.com>
2022-09-26 14:42:31 +0300

    Update PyYt.py
    update

--- PyYt.py
+++ PyYt.py
@@ -6,8 +6,11 @@ yt = YouTube(link)
 
 print("Title: ", yt.title)
 print("View: ", yt.views)
+print("Length: ", yt.length)
+print("Metadata: ", yt.metadata)
+print("Publish: ", yt.publish_date)
 
 yd = yt.streams.get_highest_resolution()
 
 # ADD WHERE YOU WANT TO GET YOUR VIDEO DOWNLOADED - DOWNLOAD PATH
-yd.download('./YT-Downloads')
+yd.download('/Users/XXX/Downloads/YT-Downloads')
